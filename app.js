e90912138236a3b5d45baa61b22fe9c543566863
Seunghyun Yeo <shyeo0517@gmail.com>
2021-09-11 16:28:39 +0900

    app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Helllllllllllo World'));
-// app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
+app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
