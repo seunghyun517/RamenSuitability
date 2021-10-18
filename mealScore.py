40575b3295a8563abd7a286576d845bcc49f488e
Seunghyun Yeo <shyeo0517@gmail.com>
2021-10-18 17:02:26 +0900

    delete data collector

--- mealScore.py
+++ mealScore.py
@@ -2,9 +2,12 @@ from menuScore import *
 import re
 
 def compileMeal(meal):
-    menus = meal.split()
-    for i in range(len(menus)):
-        menus[i] = re.compile('[가-힣]+').findall(menus[i])[0]
+    premenus = meal.split()
+    menus = []
+    for premenu in premenus:
+        tmp = re.compile('[가-힣]+').findall(premenu)
+        if len(tmp) != 0:
+            menus.append(tmp[0])
     return menus
 
 
@@ -14,6 +17,6 @@ def computeMealScore(menus):
     return sum(computeMenuScore(menu) for menu in menus) / len(menus)
 
 
-today_meal = "단호박로제스파게티①②⑤⑥⑫ 게살스프⑧ 수비드폭립⑤⑩ 모짜렐라펄샐러드② 과일요거화채② 감자부꾸미 무피클 스파클링 (밥.김치.김)"
+today_meal = "단호박로제스파게티 ①②⑤⑥⑫ 게살스프⑧ 수비드폭립⑤⑩ 모짜렐라펄샐러드② 과일요거화채② 감자부꾸미 무피클 스파클링 (밥.김치.김)"
 today_menus = compileMeal(today_meal)
 print(computeMealScore(today_menus))
